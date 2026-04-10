e9ab01a984d39b151465ba935e0d01e27750764a
Andreas Lysholm Jensen <goegl12@gmail.com>
2026-04-10 14:24:30 +0200

    add test for findItemById()

---
+++ src/test/java/Wishlist/ItemRepositoryTest.java
@@ -0,0 +1,27 @@
+package Wishlist;
+
+import Wishlist.model.Item;
+import Wishlist.repository.ItemRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Sql(scripts = "classpath:h2init.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+public class ItemRepositoryTest {
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Test
+    void findById_returnsCorrectItem(){
+        Item item = itemRepository.findItemById(1);
+
+        assertThat(item.getName()).isEqualTo("Life-size Darth Vader");
+    }
+
+
+}
