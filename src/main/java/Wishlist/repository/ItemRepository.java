18abfa943fc0cc3040c13e414e2a59e53e4e1c6f
ellidena <85340254+ellidena@users.noreply.github.com>
2026-04-10 13:09:14 +0200

    Add jdbc injection to ItemRepository

--- src/main/java/Wishlist/repository/ItemRepository.java
+++ src/main/java/Wishlist/repository/ItemRepository.java
@@ -1,7 +1,13 @@
 package Wishlist.repository;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ItemRepository {
+    private final JdbcTemplate jdbc;
+
+    public ItemRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
 }
