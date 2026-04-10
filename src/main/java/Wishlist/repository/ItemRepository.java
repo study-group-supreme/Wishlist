e7bbabbdec578b82d50c45ff675aa4cb5c705d75
Andreas Lysholm Jensen <goegl12@gmail.com>
2026-04-10 13:51:53 +0200

    added url to Item, and made getters and setters, added itemRowMapper to ItemRepository

--- src/main/java/Wishlist/repository/ItemRepository.java
+++ src/main/java/Wishlist/repository/ItemRepository.java
@@ -1,11 +1,22 @@
 package Wishlist.repository;
 
+import Wishlist.model.Item;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ItemRepository {
     private final JdbcTemplate jdbc;
+    private final RowMapper<Item> itemRowMapper = (rs, rowNum) -> {
+        Item item = new Item();
+        item.setId(rs.getInt("id"));
+        item.setName(rs.getString("title"));
+        item.setDescription(rs.getString("description"));
+        item.setPrice(rs.getLong("price"));
+        item.setUrl(rs.getString("url"));
+        return item;
+    };
 
     public ItemRepository(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
