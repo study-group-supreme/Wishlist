8dbaa5c39ec587364e582d68465ae1bcda7d6997
svanholm200-debug <svanholm200@gmail.com>
2026-04-10 12:43:12 +0200

    Added Wishlist.model class with fields & getters and setters

--- src/main/java/Wishlist/model/WishlistModel.java
+++ src/main/java/Wishlist/model/WishlistModel.java
@@ -1,4 +1,47 @@
 package Wishlist.model;
 
 public class WishlistModel {
+    private int id;
+    private String name;
+    private String description;
+    private boolean isPublic;
+
+public WishlistModel(int id, String name, String description, boolean isPublic){
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.isPublic = isPublic;
+}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(boolean aPublic) {
+        isPublic = aPublic;
+    }
 }
