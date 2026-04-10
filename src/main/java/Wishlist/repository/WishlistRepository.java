f955f8d7a8f441f3fe0846c99123a41de92349f5
ellidena <85340254+ellidena@users.noreply.github.com>
2026-04-10 13:05:40 +0200

    Add JdbcTemplate and mySQL driver dependency to pom.xml, and jdbcTemplate injection to WishlistRepository

--- src/main/java/Wishlist/repository/WishlistRepository.java
+++ src/main/java/Wishlist/repository/WishlistRepository.java
@@ -1,7 +1,14 @@
 package Wishlist.repository;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class WishlistRepository {
+    private final JdbcTemplate jdbc;
+
+    public WishlistRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
 }
