5e818321ab4b62970a235c9805aae2de1441d08b
ellidena <85340254+ellidena@users.noreply.github.com>
2026-04-10 13:07:27 +0200

    Add jdbcTemplate injection to MemberRepository

--- src/main/java/Wishlist/repository/MemberRepository.java
+++ src/main/java/Wishlist/repository/MemberRepository.java
@@ -1,7 +1,13 @@
 package Wishlist.repository;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MemberRepository {
+    private final JdbcTemplate jdbc;
+
+    public MemberRepository(JdbcTemplate jdbc){
+        this.jdbc = jdbc;
+    }
 }
