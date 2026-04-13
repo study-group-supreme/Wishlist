9d7d3f7b3c297f730c74d273254b6c8f82c281ca
ellidena <85340254+ellidena@users.noreply.github.com>
2026-04-13 12:43:54 +0200

    Add MemberService class and getByID

---
+++ src/main/java/wishlist/service/MemberService.java
@@ -0,0 +1,19 @@
+package wishlist.service;
+
+import org.springframework.stereotype.Service;
+import wishlist.model.Member;
+import wishlist.repository.MemberRepository;
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Member getById(int id){
+        return memberRepository.findById(id);
+    }
+
+}
