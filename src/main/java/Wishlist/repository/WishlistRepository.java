4e4cb89924ca815fedcf5f976d51554be955d2af
svanholm200-debug <svanholm200@gmail.com>
2026-04-13 09:26:59 +0200

    Added WishlistRowmapper in Wishlist repo, and added methods to find wishlist by id, title & owner_id

--- src/main/java/Wishlist/repository/WishlistRepository.java
+++ src/main/java/Wishlist/repository/WishlistRepository.java
@@ -1,6 +1,8 @@
 package Wishlist.repository;
 
+import Wishlist.model.WishlistModel;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,5 +12,26 @@ public class WishlistRepository {
     public WishlistRepository(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
     }
+    private final RowMapper<WishlistModel> wishlistRowMapper = (rs, rowNum) -> {
+        WishlistModel w = new WishlistModel();
+        w.setId(rs.getInt("id"));
+        w.setTitle(rs.getString("title"));
+        w.setDescription(rs.getString("description"));
+        w.setPublic(rs.getBoolean("is_public"));
+        w.setOwner_id(rs.getInt("member_id"));
+        return w;
+    };
 
+    public WishlistModel findWishlistById(int id){
+        String sql = "SELECT * FROM Wishlist WHERE id = ?";
+        return jdbc.queryForObject(sql, wishlistRowMapper, id);
+    }
+    public WishlistModel findWishlistByTitle(String title){
+        String sql = "SELECT * FROM Wishlist WHERE title = ?";
+        return jdbc.queryForObject(sql, wishlistRowMapper, title);
+    }
+    public WishlistModel findWishlistByOwnerId(int Owner_id){
+        String sql = "SELECT * FROM Wishlist WHERE member_id = ?";
+        return jdbc.queryForObject(sql, wishlistRowMapper, Owner_id);
+    }
 }
