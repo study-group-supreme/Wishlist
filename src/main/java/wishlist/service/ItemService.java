a9fd05821d7d2f27a3a1e5b036856fb3f49e6d7f
svanholm200-debug <svanholm200@gmail.com>
2026-04-13 13:04:46 +0200

    Added Item repo methods to ItemService

---
+++ src/main/java/wishlist/service/ItemService.java
@@ -0,0 +1,25 @@
+package wishlist.service;
+
+import org.springframework.stereotype.Service;
+import wishlist.model.Item;
+import wishlist.repository.ItemRepository;
+
+import java.util.List;
+
+@Service
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+    public Item getItemById(int id){
+        return itemRepository.findItemById(id);
+    }
+    public List<Item> getAllItems(){
+        return itemRepository.getAllItems();
+    }
+    public Item createItem(Item item){
+        return itemRepository.createItem(item);
+    }
+}
