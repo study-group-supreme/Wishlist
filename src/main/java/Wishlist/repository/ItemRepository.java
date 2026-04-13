527bb8283fa80b6097213578c11bb6ac1b5bce5d
Andreas Lysholm Jensen <goegl12@gmail.com>
2026-04-13 09:37:05 +0200

    add createItem() to itemRepository()

--- src/main/java/Wishlist/repository/ItemRepository.java
+++ src/main/java/Wishlist/repository/ItemRepository.java
@@ -3,9 +3,11 @@ package Wishlist.repository;
 import Wishlist.model.Item;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.sql.PreparedStatement;
 
 @Repository
 public class ItemRepository {
@@ -27,5 +29,30 @@ public class ItemRepository {
     public Item findItemById(int itemId){
         String sql = "SELECT * FROM item WHERE id = ?";
         return jdbc.queryForObject(sql, itemRowMapper, itemId);
+    }
+
+    public Item createItem(Item item){
+        String sql = """
+                INSERT INTO item (title, description, url, price)
+                VALUES(?,?,?,?)
+                """;
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbc.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, item.getName());
+            ps.setString(2, item.getDescription());
+            ps.setString(3, item.getUrl());
+            ps.setLong(4, item.getPrice());
+            return ps;
+        }, keyHolder);
+
+        int id = keyHolder.getKey() != null ? keyHolder.getKey().intValue() : -1;
+
+        if(id != -1){
+            return findItemById(id);
+        } else {
+            throw new RuntimeException("Item creation failed");
+        }
     }
 }
