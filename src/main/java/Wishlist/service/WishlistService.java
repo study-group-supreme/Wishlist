cdd364140eb6329c7e68d2826c795b4f89ee7430
August Skipper <funkskipper@hotmail.com>
2026-04-13 10:34:48 +0200

    Created the framework for a deleteWishlist method in the Service layer

--- src/main/java/Wishlist/service/WishlistService.java
+++ src/main/java/Wishlist/service/WishlistService.java
@@ -1,7 +1,24 @@
 package Wishlist.service;
 
+import Wishlist.model.Wishlist;
+import Wishlist.repository.WishlistRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WishlistService {
+    private final WishlistRepository wishlistRepository;
+
+    public WishlistService (WishlistRepository wishlistRepository) {
+        this.wishlistRepository = wishlistRepository;
+    }
+
+
+
+
+    public Wishlist deleteWishlist(int wishlistId) {
+        Wishlist wishlistToBeDeleted = wishlistRepository.findWishlistById(wishlistId);
+        wishlistRepository.deleteWishlist(wishlistId);
+        return wishlistToBeDeleted;
+    }
+
 }
