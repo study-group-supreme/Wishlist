6dc79a0a5f0460110b1edbb95ff9c5926e7e9c81
ellidena <85340254+ellidena@users.noreply.github.com>
2026-04-15 11:02:51 +0200

    Setup Wishlist controller

--- src/main/java/wishlist/controller/WishlistController.java
+++ src/main/java/wishlist/controller/WishlistController.java
@@ -1,8 +1,24 @@
 package wishlist.controller;
 
 import org.springframework.stereotype.Controller;
+import wishlist.model.Wishlist;
+import wishlist.service.ItemService;
+import wishlist.service.WishlistService;
 
 @Controller
 
 public class WishlistController {
+
+    private final ItemService itemService;
+    private final WishlistService wishlistService;
+
+    public WishlistController(ItemService itemService, WishlistService wishlistService){
+        this.itemService = itemService;
+        this.wishlistService = wishlistService;
+    }
+
+    // show all wishlists GET
+
+
+    // show wishlist by wishlist-id
 }
