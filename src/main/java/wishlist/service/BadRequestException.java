a79fec8a1689d4f878741e4344547025b77350ba
ellidena <85340254+ellidena@users.noreply.github.com>
2026-04-17 13:57:42 +0200

    Add note

--- src/main/java/wishlist/service/BadRequestException.java
+++ src/main/java/wishlist/service/BadRequestException.java
@@ -3,6 +3,7 @@ package wishlist.service;
 public class BadRequestException extends RuntimeException {
     // TODO: move to wishlist/exception
     // This is for WishlistService error handling
+    // TODO: Can we use a globalexceptionhandling????
     public BadRequestException(String message) {
         super(message);
     }
