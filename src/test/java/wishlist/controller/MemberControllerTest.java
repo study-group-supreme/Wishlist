63bb7eab9993dfeccd6a5d2fc9193af5e81b4721
svanholm200-debug <svanholm200@gmail.com>
2026-04-17 11:35:37 +0200

    Added shouldRegisterMemberAndRedirertToLogin method in memberControllerTest

--- src/test/java/wishlist/controller/MemberControllerTest.java
+++ src/test/java/wishlist/controller/MemberControllerTest.java
@@ -3,12 +3,16 @@ package wishlist.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import wishlist.model.Member;
 import wishlist.service.MemberService;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(MemberController.class)
@@ -27,4 +31,24 @@ class MemberControllerTest {
                 .andExpect(view().name("member-registration"))
                 .andExpect(model().attributeExists("member"));
     }
+@Test
+    void shouldRegisterMemberAndRedirectToLogin() throws Exception {
+
+        Member savedMember = new Member();
+        savedMember.setUsername("mads");
+        savedMember.setEmail("mads@test.dk");
+
+        when(service.create(any(Member.class))).thenReturn(savedMember);
+
+        mockMvc.perform(post("/member/save")
+                        .param("username", "mads")
+                        .param("email", "mads@test.dk")
+                        .param("password", "secret"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/auth/login"))
+                .andExpect(flash().attributeExists("member"));
+
+        verify(service).create(any(Member.class));
+    }
 }
+
