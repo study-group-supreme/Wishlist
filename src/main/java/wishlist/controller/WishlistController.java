c11233e84592b2fe2311fb70ee3b20fc8de30da4
ellidena <85340254+ellidena@users.noreply.github.com>
2026-04-17 10:10:30 +0200

    Add save postmapping fro creating a new wishlist, add wishlist/create-wishlist.html, add button to create new wishlist on templates/list.html

--- src/main/java/wishlist/controller/WishlistController.java
+++ src/main/java/wishlist/controller/WishlistController.java
@@ -3,10 +3,7 @@ package wishlist.controller;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import wishlist.model.Item;
 import wishlist.model.Wishlist;
 import wishlist.service.ItemService;
@@ -39,13 +36,11 @@ public class WishlistController {
     }
 
     // show wishlist by wishlist-id
-    @GetMapping("/{id}")
-    public String showOneWishlist(@PathVariable int id, Model model, HttpSession session) {
+    @GetMapping("/{wishlistId}")
+    public String showOneWishlist(@PathVariable int wishlistId, Model model) {
 
-        int memberId = (Integer) session.getAttribute("memberId");
-
-        Wishlist wishlist = wishlistService.getById(id);
-        List<Item> items = wishlistService.getItemsFromWishlistByWishlistId(id);
+        Wishlist wishlist = wishlistService.getById(wishlistId);
+        List<Item> items = wishlistService.getItemsFromWishlistByWishlistId(wishlistId);
 
         model.addAttribute("wishlist", wishlist);
         model.addAttribute("items", items);
@@ -56,7 +51,17 @@ public class WishlistController {
     @GetMapping("/new")
     public String showCreateWishlistForm(Model model){
         model.addAttribute("wishlist", new Wishlist());
-        return "wishlist/create";
+        return "wishlist/create-wishlist";
     }
 
+    @PostMapping("/save")
+    public String saveNewWishlist(@ModelAttribute Wishlist wishlist, HttpSession session){
+        int memberId = (Integer) session.getAttribute("memberId");
+
+        wishlist.setOwner_id(memberId);
+
+        wishlistService.createNewWishlist(wishlist);
+
+        return "redirect:/wishlist";
+    }
 }
