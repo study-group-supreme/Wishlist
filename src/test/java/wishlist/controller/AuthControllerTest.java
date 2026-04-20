11aa7a57e1f1b8676b5828be5c1d37c6b9def489
svanholm200-debug <svanholm200@gmail.com>
2026-04-20 09:46:49 +0200

    Added new Class AuthControllerTest and added test to displayLoginForm

---
+++ src/test/java/wishlist/controller/AuthControllerTest.java
@@ -0,0 +1,34 @@
+package wishlist.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import wishlist.service.MemberService;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(AuthController.class)
+class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private MemberService service;
+
+    @Test
+        //"(/login)"
+    void shouldDisplayLoginForm() throws Exception {
+        mockMvc.perform(get("/auth/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("/member/member-login"));
+    }
+
+}
