cb47f95d71d070917869ebb8fc6d1338d6ffbf5f
svanholm200-debug <svanholm200@gmail.com>
2026-04-20 10:06:12 +0200

    Added class PublicViewControllerTest and added test for display public homepage

---
+++ src/test/java/wishlist/controller/PublicViewControllerTest.java
@@ -0,0 +1,32 @@
+package wishlist.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import wishlist.controller.AuthController;
+import wishlist.controller.PublicViewController;
+import wishlist.service.MemberService;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(PublicViewController.class)
+class PublicViewControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private MemberService service;
+
+    @Test
+    void shouldDisplayPublicHomePage() throws Exception {
+        mockMvc.perform(get(""))
+                .andExpect(view().name("public/public-homepage"));
+    }
+}
