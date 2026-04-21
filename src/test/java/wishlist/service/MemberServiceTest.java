fc39e9c2730c96f6d0b0dc756125efd924d3f20f
ellidena <85340254+ellidena@users.noreply.github.com>
2026-04-21 23:35:37 +0200

    Add MemberServiceTest getById for invalid id case

--- src/test/java/wishlist/service/MemberServiceTest.java
+++ src/test/java/wishlist/service/MemberServiceTest.java
@@ -1,10 +1,13 @@
 package wishlist.service;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import wishlist.exception.BadRequestException;
 import wishlist.repository.MemberRepository;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class MemberServiceTest {
@@ -14,4 +17,9 @@ public class MemberServiceTest {
 
     @InjectMocks
     private MemberService memberService;
+
+    @Test
+    void getById_throwsBadRequest_whenIdInvalid() {
+        assertThrows(BadRequestException.class, () -> memberService.getById(0));
+    }
 }
