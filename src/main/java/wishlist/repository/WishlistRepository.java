23316419ac7de82bd77332d2c1dbe83c0a78944b
Andreas Lysholm Jensen <goegl12@gmail.com>
2026-04-21 20:38:05 +0200

    add insertSavedWishlist() to WishlistRepository

--- src/main/java/wishlist/repository/WishlistRepository.java
+++ src/main/java/wishlist/repository/WishlistRepository.java
@@ -3,6 +3,7 @@ package wishlist.repository;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import wishlist.model.Item;
+import wishlist.model.Member;
 import wishlist.model.Wishlist;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -137,6 +138,14 @@ public class WishlistRepository {
                 WHERE wishlist_id = ? AND item_id = ?
                 """;
         return jdbc.update(sql, note, url, price, wishlistId, itemId);
+    }
+
+    public int insertSavedWishlist(Wishlist wishlist, Member member){
+        String sql = """
+                INSERT INTO saved_wishlist (wishlist_id, member_id, owner_id)
+                VALUES (?, ?, ?)
+                """;
+        return jdbc.update(sql, wishlist.getId(), member.getId(), wishlist.getOwner_id());
     }
 
 }
