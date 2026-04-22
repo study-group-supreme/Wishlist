762aac2982a33e5ae5152b454186bd83f2a392f4
Andreas Lysholm Jensen <goegl12@gmail.com>
2026-04-22 16:14:52 +0200

    slowly introducing webconfig back

--- src/main/java/wishlist/config/WebConfig.java
+++ src/main/java/wishlist/config/WebConfig.java
@@ -1,16 +1,18 @@
-//package wishlist.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//import wishlist.interceptor.LoginInterceptor;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(new LoginInterceptor())
-//                .excludePathPatterns(
+package wishlist.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import wishlist.interceptor.LoginInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor()).addPathPatterns()
+                .excludePathPatterns("/**/*.css", "/**/*.png", "/public/**", "/auth/login", "/member/register", "/member/save");
+    }
+}
 //                // Static resources (Azure-safe)
 //                "/css/**",
 //                "/js/**",
