2b6c18676c9aa5d110047ffebbc18bf29535c8dd
ellidena <85340254+ellidena@users.noreply.github.com>
2026-04-22 13:19:20 +0200

    Edit updateWishlistItem_updatesExistingRow test to work with BigDecimal

--- src/test/java/wishlist/integration/WishlistRepositoryTest.java
+++ src/test/java/wishlist/integration/WishlistRepositoryTest.java
@@ -123,7 +123,7 @@ public class WishlistRepositoryTest {
 
         String newNote = "Updated note";
         String newUrl = "http://updated-url.com";
-        BigDecimal newPrice = 12345;
+        BigDecimal newPrice = new BigDecimal("12345.00");
 
         int rows = wishlistRepository.updateWishlistItem(wishlistId, itemId, newNote, newUrl, newPrice);
 
@@ -139,7 +139,7 @@ public class WishlistRepositoryTest {
 
         assertThat(result.get("note")).isEqualTo(newNote);
         assertThat(result.get("url")).isEqualTo(newUrl);
-        assertThat(((Number) result.get("price")).longValue()).isEqualTo(newPrice);
+        assertThat(((BigDecimal) result.get("price")).compareTo(newPrice)).isEqualTo(0);
     }
 
     @Test
@@ -170,7 +170,7 @@ public class WishlistRepositoryTest {
 
         String note = "JUnit note";
         String url = "http://example.com";
-        BigDecimal price = 999;
+        BigDecimal price = new BigDecimal(999);
 
         int rows = wishlistRepository.addItemToWishlist(wishlistId, newItemId, note, url, price);
 
