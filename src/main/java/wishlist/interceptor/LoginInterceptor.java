e1d92332c9aa5fa7c8d1191c220e6cb478e1b08b
ellidena <85340254+ellidena@users.noreply.github.com>
2026-04-22 15:30:18 +0200

    Edit loginInterceptor

--- src/main/java/wishlist/interceptor/LoginInterceptor.java
+++ src/main/java/wishlist/interceptor/LoginInterceptor.java
@@ -9,6 +9,11 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String path = request.getRequestURI();
+
+        if (path.startsWith("/auth/login") ||  path.startsWith("/member/register") || path.startsWith("/public/")) {
+            return true;
+        }
         HttpSession session = request.getSession(false);
 
         if(session == null){
@@ -25,4 +30,6 @@ public class LoginInterceptor implements HandlerInterceptor {
 
         return true;
     }
+
+
 }
